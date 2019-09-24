732b9f098725c3ee55246f703e60d7355187e5da
Camilo Olivo <camilo.olivo1990@gmail.com>
2019-09-24 11:49:35 -0500

    Subir al repositorio olivo1990

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hola mundo!';
+  nombre:string = "Camilo Javier";
+  apellido:string = "Olivo Sierra";
+}
